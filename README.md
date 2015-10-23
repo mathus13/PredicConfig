ceac863e7ae01bbb0ad736a3f78b7fbe1091d321
Shawn Barratt <sbarratt@workflowbydesign.com>
2015-10-23 15:39:53 -0400

    Documentation and better hook integration

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# PredicConfig
-Simple hook based config system using PRedis
+# SimpleConfig #
+
+Simple hook based config system using with a cached backend
+
+Requires 
+ - [Ethereal/SimpleCache](https://github.com/mathus13/SimpleCache)
+ - [Ethereal/SimpleHooks](https://github.com/mathus13/SimpleHooks)
+
+For uncreased usability and de-cuppleing these requirements will be reduced to an interface
+
+At construction, hooks are used to add config elements. After that you can use the ->set method, passing the key, and value.
+
+ To add items for on load, add a listener to the hook '\Ethereal\Config\Build' and append your key to the array passed in. 
+
+####Example####
+
+    class Listener 
+    {
+    	public function configListener($config)
+    	{
+    		$config['key'] = array(
+				'subKey' => 'value'
+    		);
+    		return $config;
+    	}
+    }
--- lib/Config.php
+++ lib/Config.php
@@ -3,17 +3,31 @@ namespace Ethereal;
 
 use DirectoryIterator;
 use Ethereal\Cache;
+use Ethereal\Hooks;
 
+/**
+ * Class to hold and retrieve config data from a cached service
+ * @author Shawn Barratt
+ * @see Ethereal\Cache
+ * @see Ethereal\Hooks
+ */
 class Config
 {
     
     private $config;
     protected $cache;
+    protected $hooks;
     protected $dir = __DIR__.'/config/';
-    protected $key = 'Ethereal\Config';
+    protected $key = '\Ethereal\Config';
     
-    public function __construct(Ethereal\Cache $cache)
+    /**
+     * Build Config object
+     * @param Ethereal\Cache $cache [description]
+     * @param Ethereal\Hooks $hooks [description]
+     */
+    public function __construct(Ethereal\Cache $cache, Ethereal\Hooks $hooks)
     {
+        $this->hooks = $hooks;
         $this->cache = $cache;
         $this->config = $this->getConfig();
     }
@@ -26,31 +40,25 @@ class Config
         if (!is_dir($this->dir)) {
             throw new \Exception('Invalid Config Directory: '.$this->dir);
         }
-        $config = array();
-        $files = array();
-        foreach (new \DirectoryIterator($this->dir) as $file) {
-            if (strpos($file->getFilename(), '.json') && $file->isReadable()) {
-                $files[$file->getFilename()] = $file->getPathname();
-            }
-        }
-        ksort($files);
-        foreach ($files as $name => $path) {
-            $h = fopen($path, 'r+');
-            $json = fread($h, 2048);
-            if ($parse = json_decode($json)) {
-                foreach ($parse as $k => $v) {
-                    $config[$k] = $v;
-                }
-            } else {
-                throw new \Exception('Invalid Config: '.$json);
-            }
-        }
+        $config = $this->hooks->fire("{$this->key}build", array());
         $this->cache->set($this->key, $config);
         return $config;
     }
 
+    /**
+     * Set a config item
+     * @param string $name [description]
+     * @param any $data [description]
+     */
+    public function set($name, $data)
+    {
+        $this->config[$name] = $data;
+        $this->cache->set($this->key, $config);
+    }
+
     public function get($name)
     {
+        $name = "{$this->key}{$name}";
         if (isset($this->config[$name])) {
             return $this->config[$name];
         }
